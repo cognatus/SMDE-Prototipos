cb3c703bee92af8c5c904f495eeefc1bf0bd6cb6
AlexVanHell <alejandrodnl.cv@hotmail.com>
2015-09-12 22:29:44 -0500

    Perfil, Foro , Ajustes

--- public/js/viewscontrol.js
+++ public/js/viewscontrol.js
@@ -6,12 +6,6 @@ $(document).ready(function(){
 		$('body').css('overflow' , 'hidden');
 	});
 
-	$('body').on('swipeleft' , function(){
-		$('#menu').animate({ left : '0' } , 300);
-		$('.back_menu').show();
-		$('body').css('overflow' , 'hidden');
-	});
-
 	$('.back_menu').click(function(){
 		$('#menu').animate({ left : -($('#menu').width() + 10) } , 300);
 		$(this).hide();
@@ -20,21 +14,22 @@ $(document).ready(function(){
 
 	$('.op_bg').css('top' , ( $(window).height() - $('.op_bg').height() ) * 0.5 + 'px');
 
-	$('#m_menu').click(function(){
-		$('.m_hiddenmenu').slideDown(300);
+	$('.minimenu').click(function(){
+		$(this).siblings('.minimenu_hidden').slideDown(300);
 	});
 
 	$('html , body').click(function(){
-		$('.m_hiddenmenu').slideUp(300);
-	});
-
-	$('.m_hiddenmenu , #m_menu').click(function(){
-		event.stopPropagation();
+		$('.minimenu_hidden').slideUp(300);
+		$('.minimenu_hidden , .minimenu').click(function(){
+			event.stopPropagation();
+		});
 	});
 
 	$('.block_containermsm .hover').click(function(){
-		$('.block_containermsm').animate({ scrollLeft: $('.halfgrid').width() });
-		$('#new_item').hide();
+		if($(window).width() < 820){
+			$('.block_containermsm').animate({ scrollLeft: $('.halfgrid').width() });
+			$('#new_item').hide();
+		}
 		//$('.top_pallete').hide();
 		//$('.topspace').hide();
 	});
@@ -46,5 +41,7 @@ $(document).ready(function(){
 		//$('.top_pallete').show();
 		//$('.topspace').show();
 	});
+
+	$('#signatureprogress').css('width' , parseFloat($('#signaturegralprom').text()) * 10 + '%' );
 
 });
